0f8ee3377dbbc5789c88b6c130ae4c2f9ce61e1a
John Jairo Rojas <79581520+JohnRojas06289@users.noreply.github.com>
2024-08-24 22:51:54 -0500

    arreglo de bloqueo de flechas

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-const body = document.body
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
-let activeSlide = 0
+let activeSlide = 0;
 
 rightBtn.addEventListener('click', () => {
-  activeSlide++
-
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
+  activeSlide++;
+  updateCarousel();
+});
 
 leftBtn.addEventListener('click', () => {
-  activeSlide--
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
-setBgToBody()
+  activeSlide--;
+  updateCarousel();
+});
 
 function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+  body.style.backgroundImage = slides[activeSlide].querySelector('img').src;
 }
 
 function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[activeSlide].classList.add('active');
 }
+
+function updateCarousel() {
+  setBgToBody();
+  setActiveSlide();
+  updateArrows();
+}
+
+function updateArrows() {
+  if (activeSlide === 0) {
+    leftBtn.disabled = true;
+  } else {
+    leftBtn.disabled = false;
+  }
+
+  if (activeSlide === slides.length - 1) {
+    rightBtn.disabled = true;
+  } else {
+    rightBtn.disabled = false;
+  }
+}
+
+// Inicializar el estado de las flechas y la imagen de fondo
+updateCarousel();
